04e3f377913b6f2685a3b9e5b64799cf7a61e391
a-1-us <tanusharma7597@gmail.com>
2026-05-06 16:34:40 +0530

    Data Transformation

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,9 @@
 import os
 import sys
 
-import numpy as np 
+import numpy as np
 import pandas as pd
-import dill
+import dill  ##helps to create a pickle file
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
