3f505a641e76f0cc77aa53a9545318b11036bab8
Tanu_sharma <tanusharma7597@gmail.com>
2026-05-12 14:01:59 +0530

    Update predict_pipeline.py

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,48 +1,42 @@
 import os
 import sys
 import pandas as pd
-
 from src.exception import CustomException
 from src.utils import load_object
 
-
 class PredictPipeline:
-
     def __init__(self):
         pass
 
     def predict(self, features):
-
         try:
-            model_path = os.path.join("artifacts", "model.pkl")
-            preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
+            working_dir = os.getcwd()
+            model_path = os.path.join(working_dir, "artifacts", "model.pkl")
+            preprocessor_path = os.path.join(working_dir, "artifacts", "preprocessor.pkl")
 
+            print("Before Loading")
             model = load_object(file_path=model_path)
             preprocessor = load_object(file_path=preprocessor_path)
+            print("After Loading")
 
             data_scaled = preprocessor.transform(features)
-
             preds = model.predict(data_scaled)
-
             return preds
-
+        
         except Exception as e:
             raise CustomException(e, sys)
 
-
 class CustomData:
-
     def __init__(
         self,
-        gender,
-        race_ethnicity,
-        parental_level_of_education,
-        lunch,
-        test_preparation_course,
-        reading_score,
-        writing_score
+        gender: str,
+        race_ethnicity: str,
+        parental_level_of_education: str,
+        lunch: str,
+        test_preparation_course: str,
+        reading_score: int,
+        writing_score: int
     ):
-
         self.gender = gender
         self.race_ethnicity = race_ethnicity
         self.parental_level_of_education = parental_level_of_education
@@ -52,7 +46,6 @@ class CustomData:
         self.writing_score = writing_score
 
     def get_data_as_data_frame(self):
-
         try:
             custom_data_input_dict = {
                 "gender": [self.gender],
@@ -61,7 +54,7 @@ class CustomData:
                 "lunch": [self.lunch],
                 "test_preparation_course": [self.test_preparation_course],
                 "reading_score": [self.reading_score],
-                "writing_score": [self.writing_score]
+                "writing_score": [self.writing_score],
             }
 
             return pd.DataFrame(custom_data_input_dict)
